01488baee0aa1c02816477f37d55518878bd24bd
Mohd Yousuf <mdyousuf43201@gmail.com>
2024-01-15 02:58:37 +0530

    new file added

--- loops.js
+++ loops.js
@@ -174,19 +174,19 @@ console.log(average);
 
 
 
-  // Coding Challenge #3
-  /* 
+// Coding Challenge #3
+/* 
 Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
 
 1. Create an array 'events' of the different game events that happened (no duplicates)
 2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
 3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
 4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
-      [FIRST HALF] 17: ⚽️ GOAL
+    [FIRST HALF] 17: ⚽️ GOAL
 
 GOOD LUCK 😀
 */
-
+/*
 const gameEvents = new Map([
   [17, '⚽️ GOAL'],
   [36, '🔁 Substitution'],
@@ -201,11 +201,61 @@ const gameEvents = new Map([
   [92, '🔶 Yellow card'],
 ]);
 
-
-const events = new Set;
+// 1 - solution
+const events = [...new Set(gameEvents.values())];
+// console.log(gameEvents.entries());
+// for (const [key, value] of gameEvents.entries()) {
+//   events.add(value);
+// }
 console.log(events);
-// events.add(Object.entries(gameEvents.values()));
-console.log(gameEvents.get());
 
+
+// 2 - solution
 gameEvents.delete(64);
 console.log(gameEvents);
+
+
+// 3 - solution
+const time = [...gameEvents.keys()].pop(); // just a bonus
+// console.log(time);
+console.log(`An event happened, on average, every ${ time / gameEvents.size} minutes`);
+
+// 4 solution
+for (const [key, value] of gameEvents.entries()) {
+  const str = key <= 45 ? '[First half]' : '[Second half]';
+    console.log(`${str} ${key} : ${value}`);
+  // console.log(`[Second Half] ${key} : ${value}`);
+} 
+*/
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
